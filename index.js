f39542630b0d8297deff04855e5cb1da2ef3c7b5
Deryn Brown <Deryn.2017@gmail.com>
2022-08-29 17:53:00 -0500

    commit tree 557e90b590dd7b9e61d65e83dee2387858f8f89e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2)
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22;
+let num4 = 18;
+let mod = (num3 % num4);
+
+function randomFunction() {
+    return Math.floor(Math.random() * 10) + 1
+}
+
+let max = Math.max(0, 5, 10, 15, 20);
